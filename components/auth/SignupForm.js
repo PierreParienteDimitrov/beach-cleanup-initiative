2b730529b0e3b533f6b209ab970fe2674b98648a
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-06-29 16:29:09 -0700

    added input validation, styling and background picture

--- components/auth/SignupForm.js
+++ components/auth/SignupForm.js
@@ -1,21 +1,10 @@
 import { useRef, useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import footprint from '../../images/footprint.jpg';
 import { validateEmail } from '../../util/validations';
 import SignupBtn from '../buttons/SignupBtn';
 
 async function createUser(name, email, password) {
 	console.log(email);
-
-	// adding validation
-	const emailValidation = await validateEmail(email);
-	console.log(emailValidation);
-
-	if (!emailValidation) {
-		// if is not email, return true to setAlert to true
-		return true;
-	}
 
 	const response = await fetch('api/auth/create-user', {
 		method: 'POST',
@@ -41,7 +30,9 @@ const SignupForm = () => {
 	const passwordRef = useRef();
 
 	// States
-	const [alert, setAlert] = useState(false);
+	const [nameAlert, setNameAlert] = useState(false);
+	const [emailAlert, setEmailAlert] = useState(false);
+	const [passwordAlert, setPasswordAlert] = useState(false);
 
 	const submitHandler = async (e) => {
 		e.preventDefault();
@@ -50,32 +41,41 @@ const SignupForm = () => {
 		const password = passwordRef.current.value;
 
 		// Reinitializing alert to false
-		setAlert(false);
+		setNameAlert(false);
+		setEmailAlert(false);
+		setPasswordAlert(false);
 
-		const isTrue = await createUser(name, email, password);
-		setAlert(isTrue);
+		// Name validation
+		if (!name) {
+			setNameAlert(true);
+		}
+
+		// Email validation
+		const emailValidation = await validateEmail(email);
+		if (!emailValidation) {
+			// if is not email, return true to setAlert to true
+			return setEmailAlert(true);
+		}
+
+		// Password validation
+		if (!password) {
+			setPasswordAlert(true);
+		}
+
+		const result = await createUser(name, email, password);
 	};
 
 	return (
 		<div className='w-full h-screen flex flex-col justify-center items-center'>
-			<Image
-				className='w-screen h-screen -z-10'
-				src={footprint}
-				alt='Yellow footprint'
-				layout='fill'
-				objectFit='cover'
-				objectPosition='center'
-			/>
-
 			<div className='mb-8 text-center'>
 				<h3>Create your Free Account</h3>
 
-				<h6>
-					Already have an account?{' '}
+				<div className='flex items-center'>
+					<h5 className='mr-1'>Already have an account?</h5>
 					<Link href='/login'>
-						<a>Sign In</a>
+						<a className='text-blue-600'>Sign In</a>
 					</Link>
-				</h6>
+				</div>
 			</div>
 
 			{/* Form */}
@@ -88,10 +88,13 @@ const SignupForm = () => {
 							type='name'
 							id='name'
 							placeholder='Your full name'
-							required
+							// required
 							ref={nameRef}
 							className='border p-2 rounded-md'
 						/>
+						{nameAlert && (
+							<h6 className='text-red-500'>Please enter your full name</h6>
+						)}
 					</div>
 					{/* Email */}
 					<div className='flex flex-col mb-6'>
@@ -100,11 +103,11 @@ const SignupForm = () => {
 							// type='email'
 							id='email'
 							placeholder='you@email.com'
-							required
+							// required
 							ref={emailRef}
 							className='border p-2 rounded-md'
 						/>
-						{alert && <p>Please enter valid email</p>}
+						{emailAlert && <h6 className='text-red-500'>Please enter valid email</h6>}
 					</div>
 					{/* Password */}
 					<div className='flex flex-col mb-10'>
@@ -113,10 +116,13 @@ const SignupForm = () => {
 							type='password'
 							id='password'
 							placeholder='Enter a strong password'
-							required
+							// required
 							ref={passwordRef}
 							className='border p-2 rounded-md'
 						/>
+						{passwordAlert && (
+							<h6 className='text-red-500'>Please enter a password</h6>
+						)}
 					</div>
 
 					<SignupBtn cta={'Sign Up'} />
--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,20 @@
 import SignupForm from '../components/auth/SignupForm';
+import Image from 'next/image';
+import footprint from '../images/footprint.jpg';
 
 const signup = () => {
 	return (
 		<>
-			<SignupForm />
+			<Image
+				src={footprint}
+				alt='Yellow footprint'
+				layout='fill'
+				objectFit='cover'
+				objectPosition='center'
+			/>
+			<div className='relative'>
+				<SignupForm />
+			</div>
 		</>
 	);
 };
